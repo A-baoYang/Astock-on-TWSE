3e38f98f85d6427735d8273fb875dd148d600f69
A-baoYang <rubyyang0615@gmail.com>
2022-07-18 03:41:27 +0000

    update: add check mechanism to avoid repeat fetch from finlab free coda

--- get_factors.py
+++ get_factors.py
@@ -14,8 +14,24 @@ stock_id = parser_args.stock_id
 start = parser_args.start
 end = parser_args.end
 stock_filepath = f"/home/jovyan/graph-stock-pred/Astock/data/raw/yf_daily/{stock_id}.TW.csv"
-basic_aspect_filepath = os.path.join("/home/jovyan/graph-stock-pred/Astock/data/raw/finlab", f"{stock_id}-daily_financial_factors.csv")
+finlab_dir = "/home/jovyan/graph-stock-pred/Astock/data/raw/finlab"
+# 上市櫃市場成交資訊 及 基本面-個股日本益比、殖利率及股價淨值比
+finlab_dataset_names = ["price:成交股數","price:成交筆數","price:成交金額","price_earning_ratio:殖利率(%)","price_earning_ratio:本益比","price_earning_ratio:股價淨值比"]
+basic_aspect_filepath = os.path.join(finlab_dir, f"{stock_id}-daily_financial_factors.csv")
 technical_aspect_filepath = os.path.join("/home/jovyan/graph-stock-pred/Astock/data/raw/talib", f"{stock_id}-technique_indicators.csv")
+
+
+def finlab_login():
+
+    import finlab
+    from finlab import data
+
+    config = configparser.ConfigParser()
+    config.read_file(open("secret.cfg"))
+    api_token = config.get("FINLAB", "API_TOKEN")
+    finlab.login(api_token=api_token)
+    return data
+
 
 
 if __name__ == "__main__":
@@ -23,34 +39,32 @@ if __name__ == "__main__":
     # check if data has been downloaded before
     if os.path.exists(basic_aspect_filepath):
         print(f"Data already fetched at {basic_aspect_filepath}")
+    
     else:
-
-        import finlab
-        from finlab import data
-
-        # login
-        config = configparser.ConfigParser()
-        config.read_file(open("secret.cfg"))
-        api_token = config.get("FINLAB", "API_TOKEN")
-        finlab.login(api_token=api_token)
-
-        # 上市櫃市場成交資訊
+        
         df = pd.DataFrame()
-        for col in tqdm(["成交股數","成交筆數","成交金額"]):
-            dataset = data.get(f"price:{col}")
-            dataset = dataset[(dataset.index>=start) & (dataset.index<end)][[stock_id]]
-            dataset.columns = [col]
-            df = pd.concat([df, dataset], axis=1)
+        for dataname in tqdm(finlab_dataset_names):
+            filepath = os.path.join(finlab_dir, dataname.replace(":","_") + ".csv")
+            # check if finlab data up-to-date
+            if os.path.exists(filepath):
+                dataset = pd.read_csv(filepath)
+                dataset = dataset.set_index("date")
+                if start >= dataset.index.min() and end <= dataset.index.max():
+                    pass
+                else:
+                    data = finlab_login()
+                    dataset = data.get(dataname)
+                    dataset.to_csv(filepath)
+            else:
+                data = finlab_login()
+                dataset = data.get(dataname)
+                dataset.to_csv(filepath)
 
-        # 基本面
-        # 個股日本益比、殖利率及股價淨值比
-        for col in tqdm(["殖利率(%)","本益比","股價淨值比"]):
-            dataset = data.get(f"price_earning_ratio:{col}")
             dataset = dataset[(dataset.index>=start) & (dataset.index<end)][[stock_id]]
-            dataset.columns = [col]
+            dataset.columns = [dataname.split(":")[-1]]
             df = pd.concat([df, dataset], axis=1)
-
-        df.to_csv(daily_factor_filepath)
+            
+        df.to_csv(basic_aspect_filepath)
         print(f"Stock {stock_id} basic aspect factors fetched")
 
 
@@ -66,7 +80,9 @@ if __name__ == "__main__":
     # check if data has been downloaded before
     if os.path.exists(technical_aspect_filepath):
         print(f"Data already fetched at {technical_aspect_filepath}")
+    
     else:
+        
         if os.path.exists(stock_filepath):
 
             import talib
